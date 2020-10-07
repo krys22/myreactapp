b5d4be8c0dc0005bd87735755d16d34aa658c840
Mac <mac@Chris-MacBook-Pro.local>
2020-10-07 23:31:43 +0100

    add super constructor

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 
 class Application extends Component{
 
+    constructor(props){
+        super(props);
+    }
+
+
     render(){
 
         let name = 'Maryam';
