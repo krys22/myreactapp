a381b4cb12096af1092b5f980f62b793e1f880e6
Mac <mac@Chris-MacBook-Pro.local>
2020-10-07 22:09:48 +0100

    first react commit

---
+++ src/Application.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Application extends Component{
+
+    render(){
+
+        let name = 'Maryam';
+
+        return (<div>
+            <h1>My first react app is dedicated to {name}</h1>
+            
+            </div>);
+    }
+    
+}
+
+export default Application;
